c42b95eb24905a0c98ae0324bad3f9d8dc485eef
Trần Văn Mỹ <tvmptit0511@gmail.com>
2026-06-17 10:43:36 +0700

    Add bai2/storage/__init__.py

---
+++ bai2/storage/__init__.py
@@ -0,0 +1,6 @@
+"""
+Package `storage`
+------------------
+Chứa các module xử lý lưu trữ vật lý: tính toán dung lượng/block
+(disk_manager.py) và quản lý hệ thống thư mục an toàn (io_helper.py).
+"""
